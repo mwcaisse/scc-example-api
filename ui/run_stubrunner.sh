fc62fe45a46b31c7875db3006fef0a764b82b21e
Mitchell Caisse <mitchell@mwcaisse.com>
2021-01-24 14:30:43 -0500

    Downloading stubrunner if it doesn't exist

--- ui/run_stubrunner.sh
+++ ui/run_stubrunner.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Stubrunner download URL
-# https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-contract-stub-runner-boot/3.0.0/spring-cloud-contract-stub-runner-boot-3.0.0.jar
+
+# If stubrunner jar doesn't exist, fetch it
+if [ ! -f ./stubrunner.jar ]; then
+  wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-contract-stub-runner-boot/3.0.0/spring-cloud-contract-stub-runner-boot-3.0.0.jar -O stubrunner.jar
+fi
 
 java -jar \
   -Dstubrunner.generateStubs=true \
   -Dstubrunner.repositoryRoot=git://git@github.com:mwcaisse/scc-example-api-ui.git \
   -Dstubrunner.stubsMode=REMOTE \
   -Dstubrunner.ids=com.mwcaisse.samples:recipe-api:+:8950 \
-  spring-cloud-contract-stub-runner-boot-3.0.0.jar
+  ./stubrunner.jar
